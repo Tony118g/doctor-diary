30af132fb31d4f744f083d416c82f11582e37639
Tony118g <guimaraesantony@gmail.com>
2022-11-04 14:35:35 +0000

    Add main variables to get data from worksheets

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+"""
+Main file to run the application.
+"""
+
+# Import libraries.
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Define the APIs used by the program.
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,8 +18,10 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("doctor's_diary")
 
+# Variables for each sheet in the spreadsheet.
 appointments = SHEET.worksheet("appointments")
+visit_history = SHEET.worksheet("visit_history")
 
-all_appointments = appointments.get_all_values()
-
-print(all_appointments)
+# Variables for all data in each sheet.
+all_appts = appointments.get_all_values()
+all_visits = visit_history.get_all_values()
