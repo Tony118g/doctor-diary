80d57a820a5d5a071b9a8ef67b0e932ff68dee17
Tony118g <guimaraesantony@gmail.com>
2022-11-08 15:19:37 +0000

    Fix bug with current_date variable in wrong format
    Add variable to change and store current_date in required format.

--- run.py
+++ run.py
@@ -27,7 +27,10 @@ appointments = SHEET.worksheet("appointments")
 all_appts = appointments.get_all_values()
 
 # Global variables used in multiple functions.
+
 current_date = datetime.date.today()
+# Places the current date into the correct format for the program.
+current_date_fmted = datetime.datetime.strftime(current_date, "%d/%m/%Y")
 
 
 def display_records(records, topic, heads):
@@ -269,7 +272,7 @@ def search_date(specification):
     """
     
     if specification == "today":
-        search_dte = datetime.datetime.strftime(current_date, "%d/%m/%Y")
+        search_dte = current_date_fmted
         date_desc = "today"
     elif specification == "search":
         search_dte = get_date()
@@ -303,7 +306,7 @@ def get_avail_times(data):
                   ]
     unav_times = get_appts_for_date(data, "booked_times")
     av_times = [time for time in appt_times if time not in unav_times]
-    if data == current_date:
+    if data == current_date_fmted:
         current_time = datetime.datetime.now().strftime("%H%M")
         today_av_times = [time for time in av_times if time > current_time]
         return today_av_times
